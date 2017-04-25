fbc7de1d379c74141e980b9c79e22ea2b6553acb
Victor Mihaita <vmihai12@icloud.com>
2017-04-25 18:43:21 +0200

    Comments done
    -Design
    -Functionality

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var Comment = require("./models/comment");
 mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
-
+app.use(express.static(__dirname + "/public"));
 seedDB();
 
 app.get("/", function(req, res){
--- seeds.js
+++ seeds.js
@@ -6,27 +6,27 @@ var Campground = require("./models/campground");
 var data = [
   {
     name:"Bucegi",
-    image:"https://ro.wikipedia.org/wiki/Fișier:Vedere_panoramica.JPG",
+    image:"http://wallpapere.org/wp-content/uploads/2011/06/wallpapere-munti-natura.jpg",
     description:"asd asd asd as fas fsfas fas fsa fa sfa sf asf"},
   {
     name:"Fagaras",
-    image:"https://ro.wikipedia.org/wiki/Fișier:PozeDiham_18.05.2008_(4).JPG",
+    image:"http://www.util21.ro/relaxare/poze/munte/hymalaya.jpg",
     description:"asd asd asd as fas fsfas fas fsa fa sfa sf asf"},
   {
     name:"Godeanu",
-    image:"https://ro.wikipedia.org/wiki/Fișier:Bucegi_2009-7_5.jpg",
+    image:"http://wallpapere.org/wp-content/uploads/2011/05/munte_inzapezit_poiana_insorita_hd.jpg",
     description:"asd asd asd as fas fsfas fas fsa fa sfa sf asf"},
   {
     name:"Negoiu",
-    image:"https://ro.wikipedia.org/wiki/Fișier:Cascada_Spumoasa_Bucegi.jpg",
+    image:"http://muntii-bucegi.ro/img/Bucegi_vedere.jpg",
     description:"asd asd asd as fas fsfas fas fsa fa sfa sf asf"},
   {
     name:"Vama Veche",
-    image:"https://ro.wikipedia.org/wiki/Fișier:Varful_Bucsoiu,_2492m,_Muntii_Bucegi.jpg",
+    image:"http://store03-1.peteava.ro/c1d1/da/fb/382532.jpg?token=ebe2544b23becb96",
     description:"asd asd asd as fas fsfas fas fsa fa sfa sf asf"},
   {
     name:"Mamaia",
-    image:"https://ro.wikipedia.org/wiki/Fișier:Floare_de_altitudine_in_Muntii_Bucegi.jpg",
+    image:"http://www.peisaje-montane.ro/poze_peisaje_wallpaper_bucegi_valea_prahovei/imagini_desktop/poze_munte_medii/medii-bucegi-valea-prahovei-47.jpg",
     description:"asd asd asd as fas fsfas fas fsa fa sfa sf asf"},
 ]
 
